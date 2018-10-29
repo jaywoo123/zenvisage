1d3f532deb1f4b06e297206954ca24b695069cd7
Jaewoo Kim <jaewookim@host-4-243.ilcujsm.champaign.il.us.clients.pavlovmedia.net>
2018-10-28 23:34:45 -0500

    test headless zenvisage e2e test 1st time

--- build.sh
+++ build.sh
@@ -8,3 +8,9 @@ if [[ "$?" -ne 0 ]] ; then
   echo 'mvn install failed. Exited with non-zero value '; exit 42
 fi
 cp lib/*.jar target/zenvisage/WEB-INF/lib/.
+
+sh run.sh
+sleep 10 # give zenvisage some time to start
+cd src/main/webapp
+webdriver-manager start
+protractor conf.js
