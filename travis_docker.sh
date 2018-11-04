8edf43378408e8762639c053e433c9265b6a7203
Jaewoo Kim <jaewookim@host-1-81.ilur711elm.urbana.il.us.clients.pavlovmedia.net>
2018-11-04 00:02:23 -0500

    changed order

--- travis_docker.sh
+++ travis_docker.sh
@@ -3,9 +3,8 @@ cd docker
 docker-compose up &
  # give zenvisage some time to start
 sleep 50
-sh scripts/run-test.sh
 cd .. 
-
+sh scripts/run-test.sh
 #start selenium and protractor 
 cd src/main/webapp/test
 npx webdriver-manager update --gecko=false
