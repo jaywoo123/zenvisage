8ba5116b73ac7a5877c31259173207ec376be66b
Jaewoo Kim <jaewookim@host-1-81.ilur711elm.urbana.il.us.clients.pavlovmedia.net>
2018-11-04 00:17:34 -0500

    adding chrome options

--- src/main/webapp/test/conf.js
+++ src/main/webapp/test/conf.js
@@ -2,8 +2,12 @@ exports.config = {
   framework: 'jasmine',
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['dataSmoothing.spec.js','query.spec.js','basicCheck.spec.js','dynamicClass.spec.js'],
-  capabilities: {'browserName': 'chrome', 'acceptInsecureCerts': true},
-	//capabilities: {'acceptInsecureCerts': 'true'}
+  capabilities: {'browserName': 'chrome', 
+		'acceptInsecureCerts': true,
+    		'chromeOptions': {
+      			args: ['--disable-browser-side-navigation'] 
+    		} 
+	},
   getPageTimeout: 30000,
   allScriptsTimeout: 30000
 }
