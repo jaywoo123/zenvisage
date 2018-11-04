4ff96837bf00bc149d115224a9353acbedfa6f73
Jaewoo Kim <jaewookim@host-1-81.ilur711elm.urbana.il.us.clients.pavlovmedia.net>
2018-11-04 01:09:04 -0500

    adding chrome options

--- src/main/webapp/test/conf.js
+++ src/main/webapp/test/conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   framework: 'jasmine',
-  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['dataSmoothing.spec.js','query.spec.js','basicCheck.spec.js','dynamicClass.spec.js'],
   capabilities: {'browserName': 'chrome', 
 		'acceptInsecureCerts': true,
@@ -10,5 +10,8 @@ exports.config = {
     		} 
 	},
   getPageTimeout: 300000,
-  allScriptsTimeout: 300000
+  allScriptsTimeout: 300000,
+  jasmineNodeOpts: {
+	defaultTimeoutInterval: 2500000
+  }
 }
