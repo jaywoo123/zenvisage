bfb3f9756ae8a09d7db8ebe8df154fc2411559cc
Jaewoo Kim <jaewookim@wirelessprv-10-193-172-125.near.illinois.edu>
2018-11-05 12:15:48 -0600

    added backend tests

--- travis_docker.sh
+++ travis_docker.sh
@@ -3,9 +3,9 @@
 sleep 100 
 sh scripts/run-test.sh
 #start selenium and protractor 
-cd src/main/webapp/test
-npx webdriver-manager update --gecko=false
-echo 'update finished'
-webdriver-manager start --standalone &
-protractor conf.js
+#cd src/main/webapp/test
+#npx webdriver-manager update --gecko=false
+#echo 'update finished'
+#webdriver-manager start --standalone &
+#protractor conf.js
 
