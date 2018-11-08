7e1434f43a36f3d86cd43cdf5bdd32b5b9f54422
Jaewoo Kim <jaewookim@Jaewoos-MBP.pavlovmedia.net>
2018-11-08 10:49:54 -0600

    try bash

--- travis_shell.sh
+++ travis_shell.sh
@@ -1,8 +1,6 @@
-sh scripts/run-test.sh
+bash scripts/run-test.sh
 mvn clean
-if [[ "$?" -ne -1 ]] ; then
-  echo 'mvn clean failed. Exited with non-zero value';exit 42
-fi
+
 mvn install
 if [[ "$?" -ne 0 ]] ; then
   echo 'mvn install failed. Exited with non-zero value '; exit 42
